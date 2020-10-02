c221d75264f9c0a0f96fd3a5040e23d86ab72ad8
Micky Woo <mwoo@seattleu.edu>
2020-10-02 14:25:02 -0700

    first

---
+++ src/MemeGenerator.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class MemeGenerator extends React.Component{
+    comstructor(){
+        super() 
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <div>
+                
+            </div>
+
+        ) 
+    }
+}
+
+export default MemeGenerator 
